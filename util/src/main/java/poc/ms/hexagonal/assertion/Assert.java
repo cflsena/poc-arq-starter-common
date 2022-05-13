fb852f125c7279c67636da438a26a6a8e48ab35b
Cleiton Sena <cflsena2@gmail.com>
2022-05-13 19:35:22 -0300

    Assertions

--- util/src/main/java/poc/ms/hexagonal/assertion/Assert.java
+++ util/src/main/java/poc/ms/hexagonal/assertion/Assert.java
@@ -1,4 +1,81 @@
 package poc.ms.hexagonal.assertion;
 
-public class Assert {
+import lombok.experimental.UtilityClass;
+import poc.ms.hexagonal.exception.BaseException;
+import poc.ms.hexagonal.message.MessageTemplate;
+
+import java.util.Collection;
+import java.util.Map;
+
+@UtilityClass
+public class Assert<T extends BaseException> {
+
+    public static void isTrue(final boolean condition, final MessageTemplate messageTemplate) {
+        if (!condition) {
+            final var ex = getThrowInstance(messageTemplate);
+            throwException(ex);
+        }
+    }
+
+    public static void isFalse(boolean condition, MessageTemplate messageTemplate) {
+        if (condition) {
+            final var ex = getThrowInstance(messageTemplate);
+            throwException(ex);
+        }
+    }
+
+    public static void isNull(Object value, MessageTemplate messageTemplate) {
+        if (value != null) {
+            final var ex = getThrowInstance(messageTemplate);
+            throwException(ex);
+        }
+    }
+
+    public static void isNotNull(Object value, MessageTemplate messageTemplate) {
+        if (value == null) {
+            final var ex = getThrowInstance(messageTemplate);
+            throwException(ex);
+        }
+    }
+
+    public static void isNotEmpty(String value, MessageTemplate messageTemplate) {
+        if (value == null || value.trim().length() == 0) {
+            final var ex = getThrowInstance(messageTemplate);
+            throwException(ex);
+        }
+    }
+
+    public static void isNotEmpty(Collection<?> value, MessageTemplate messageTemplate) {
+        isNotNull(value, messageTemplate);
+        if (value.isEmpty()) {
+            final var ex = getThrowInstance(messageTemplate);
+            throwException(ex);
+        }
+    }
+
+    public static void isNotEmpty(Map<?, ?> value, MessageTemplate messageTemplate) {
+        isNotNull(value, messageTemplate);
+        if (value.isEmpty()) {
+            final var ex = getThrowInstance(messageTemplate);
+            throwException(ex);
+        }
+    }
+
+    public static <T> void isNotEmpty(T[] value, MessageTemplate messageTemplate) {
+        isNotNull(value, messageTemplate);
+        if (value.length == 0) {
+            final var ex = getThrowInstance(messageTemplate);
+            throwException(ex);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T extends BaseException> T getThrowInstance(final MessageTemplate messageTemplate) {
+        return (T) new BaseException(messageTemplate);
+    }
+
+    private static <T extends BaseException> void throwException(final T ex) throws T {
+        throw ex;
+    }
+
 }
--- util/src/main/java/poc/ms/hexagonal/exception/BaseException.java
+++ util/src/main/java/poc/ms/hexagonal/exception/BaseException.java
@@ -7,19 +7,19 @@ import poc.ms.hexagonal.message.MessageTemplate;
 import java.io.Serial;
 
 @Getter
-public abstract class BaseException extends RuntimeException {
+public class BaseException extends RuntimeException {
 
     @Serial
     private static final long serialVersionUID = 5017800280141188329L;
     private final Error error;
 
-    protected BaseException(final MessageTemplate messageTemplate) {
+    public BaseException(final MessageTemplate messageTemplate) {
         super(messageTemplate.message(), null, true, false);
         this.error = new Error();
         this.error.add(messageTemplate.code(), messageTemplate.message());
     }
 
-    protected BaseException(final Error error) {
+    public BaseException(final Error error) {
         super(error.allMessagesConcatenated(), null, true, false);
         this.error = error;
     }
