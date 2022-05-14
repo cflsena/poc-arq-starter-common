dc12a008390f37341537c54a06707b0c9a26e4f0
Cleiton Sena <cflsena2@gmail.com>
2022-05-13 22:01:02 -0300

    Adicionando componente para tratamento de mensagens

--- util/src/main/java/poc/ms/hexagonal/error/Error.java
+++ util/src/main/java/poc/ms/hexagonal/error/Error.java
@@ -1,10 +1,13 @@
 package poc.ms.hexagonal.error;
 
+import lombok.NoArgsConstructor;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@NoArgsConstructor
 public class Error {
 
     private static final int FIRST_ELEMENT_INDEX = 0;
@@ -16,16 +19,16 @@ public class Error {
         this.errors.add(errorMessageDetail);
     }
 
+    public List<ErrorMessageDetail> getErrors() {
+        return Collections.unmodifiableList(this.errors);
+    }
+
     public void add(final ErrorMessageDetail errorMessageDetail) {
         this.errors.add(errorMessageDetail);
     }
 
     public ErrorMessageDetail first() {
         return this.errors.get(FIRST_ELEMENT_INDEX);
-    }
-
-    public List<ErrorMessageDetail> all() {
-        return Collections.unmodifiableList(this.errors);
     }
 
     public String allMessagesConcatenated() {
--- util/src/main/java/poc/ms/hexagonal/error/ErrorMessageDetail.java
+++ util/src/main/java/poc/ms/hexagonal/error/ErrorMessageDetail.java
@@ -2,17 +2,13 @@ package poc.ms.hexagonal.error;
 
 import lombok.Getter;
 
-import java.time.LocalDateTime;
-
 @Getter
 public class ErrorMessageDetail {
 
-    private final LocalDateTime dateTime;
     private final String code;
     private final String message;
 
     public ErrorMessageDetail(final String code, final String message) {
-        this.dateTime = LocalDateTime.now();
         this.message = message;
         this.code = code;
     }
